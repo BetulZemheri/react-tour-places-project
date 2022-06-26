89123681765fd5039471a219c44ae4b700931566
Betul Zemheri <a.betul2200@gmail.com>
2022-06-26 16:11:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.scss"
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar.js";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Main />
+      
+    </div>
+  )
+}
+
+export default App;
